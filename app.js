0ce1f24bc64a3dfe33f6fe8ff714460856db2978
Wirya Ramadhan <AdhanIsCuber@gmail.com>
2017-06-26 23:07:30 +0700

    admin panel

--- app.js
+++ app.js
@@ -22,9 +22,34 @@ app.get('/kategori', function(req, res, next) {
     res.render('kategori#kategori.html');
 });
 
+app.get('/search', function(req, res, next) {
+    res.render('search#search.html');
+});
+
+app.get('/404', function(req, res, next) {
+    res.render('404.html');
+});
+
+app.get('/detail', function(req, res, next) {
+    res.render('detail.html');
+});
+
+
+// route admin
+app.get('/admin', function(req, res, next) {
+    res.redirect('/admin/login');
+});
+
+app.get('/admin/login', function(req, res, next) {
+    res.render('admin/login.html');
+});
+
+app.get('/admin/dashboard', function(req, res, next) {
+    res.render('admin/index.html');
+});
 
 var server = app.listen(3000, function() {
     console.log('server start on http://127.0.0.1:3000');
-})
+});
 
 module.exports = app;
